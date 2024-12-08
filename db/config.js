8abe3b44edc524034170759c06d9ef3a0fbb0452
yashwardhancodes <yashvardhansingar@gmail.com>
2024-12-08 11:00:31 +0530

    cors

--- db/config.js
+++ db/config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 
 
 const mongoose =require("mongoose");
-// mongoose.connect("mongodb://127.0.0.1:27017/ProjectK") 
-mongoose.connect(process.env.DB_URI);
+mongoose.connect("mongodb://127.0.0.1:27017/ProjectK") 
+// mongoose.connect(process.env.DB_URI);
